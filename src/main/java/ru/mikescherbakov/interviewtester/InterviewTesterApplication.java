cd49ae5c41769751cea679c04919acb69ab2e831
ScherbakovMike <scherbakov.mike@gmail.com>
2022-02-01 11:58:30 +0300

    Initial commit

---
+++ src/main/java/ru/mikescherbakov/interviewtester/InterviewTesterApplication.java
@@ -0,0 +1,13 @@
+package ru.mikescherbakov.interviewtester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewTesterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewTesterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/mikescherbakov/interviewtester/InterviewTesterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mikescherbakov.interviewtester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewTesterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
